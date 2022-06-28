68661de7205dd210ef893d4b4605baa5a6814ec5
Jacques van der Westhuizen <jacqueswest@gmail.com>
2022-06-28 23:43:47 +0200

    feature: Added all test that is covered in RND-6160. Also added further functionality to overwrite the username and password

--- test_data/company_data.py
+++ test_data/company_data.py
@@ -7,7 +7,13 @@ class CompanyData:
                 "admin": ["UFT_AD_01", "Gators2012"],
                 "exec": ["UFT_EX_01", "Gators2012"],
                 "hm": ["UFT_HM_01", "Gators2012"],
-                "employee": ["UFT_EM_01", "Gators2012"]
+                "employee": ["UFT_EM_01", "Gators2012"],
+                "inactive": ["inactive", "Gators2012"]
             }
         }
+    }
+
+    login_validation = {
+        "credentials_error": "You have used an incorrect Username and or Password. Please review this information and try again. Or, contact your system administrator.",
+        "inactive_login_error": "Your user account has been deactivated. Please contact your SilkRoad Recruiting administrator."
     }
