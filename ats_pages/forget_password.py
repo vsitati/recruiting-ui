bc4bcacdc4aeb50013a1b4017bf59bbc610e875e
Jacques van der Westhuizen <jacqueswest@gmail.com>
2022-06-30 21:17:11 +0200

    feature: Added forget password tests. Will Add email verification on next PR

---
+++ ats_pages/forget_password.py
@@ -0,0 +1,44 @@
+from selenium.webdriver.common.by import By
+from ats_pages.base import BasePage
+from test_data.test_data_details import TestData
+
+
+class Elements:
+    username = (By.ID, 'username')
+    submit_btn = (By.ID, 'submitButton')
+    return_to_login = (By.ID, 'returnToLoginLink')
+    forget_password_instruction_text = (By.ID, "forgotPasswordInstructionText")
+    heading = (By.TAG_NAME, "H3")
+    empty_field_error = (By.ID, "username-error")
+    empty_field_validation_msg = (By.XPATH, ".//span[@class = 'help-block']")
+    acc_verification_heading = (By.XPATH, ".//span[@class = 'sr-product-logo']")
+    account_verification_text = (By.CSS_SELECTOR, ".alert.alert-info")
+
+
+class ForgetPassword(BasePage, Elements):
+    def __init__(self, driver):
+        super().__init__(driver)
+
+    def verify_instruction_text(self):
+        instruction_text = self.get_text(self.forget_password_instruction_text)
+        test_data_text = TestData.forget_password_text
+        return instruction_text == test_data_text
+
+    def forget_password_heading(self):
+        return self.driver.find_element_by_locator(self.heading).text
+
+    def verify_empty_field_error_msg(self):
+        return self.driver.find_element_by_locator(self.empty_field_validation_msg).text
+
+    def click_submit_btn(self):
+        return self.do_click(self.driver.find_element_by_locator(self.submit_btn))
+
+    def account_verification_heading(self):
+        return self.driver.find_element_by_locator(self.acc_verification_heading).text
+
+    def verify_account_verification_text(self):
+        account_verification_text = self.driver.find_element_by_locator(self.account_verification_text).text
+        print("ACC: ", account_verification_text)
+        test_data_text = TestData.account_verification_text
+        return account_verification_text == test_data_text
+
--- test_data/company_data.py
+++ test_data/company_data.py
@@ -17,3 +17,10 @@ class CompanyData:
         "credentials_error": "You have used an incorrect Username and or Password. Please review this information and try again. Or, contact your system administrator.",
         "inactive_login_error": "Your user account has been deactivated. Please contact your SilkRoad Recruiting administrator."
     }
+
+    forget_password_text = "You have requested to update your SilkRoad Recruiting password. Please provide us with the following information, so we may process your request. Once you have completed the form below and clicked the \"Submit\" button, we will send a message to the email address on file for the username provided. Please follow the directions in the message to update your password." \
+                           "\nIf you do not know your username, please contact your administrator." \
+                           "\n\nThank You"
+
+    account_verification_text = "A message has been sent to your email address. Please follow the instructions in the email message to complete the change password process." \
+                                "\nIf you have any questions, please contact your administrator."
