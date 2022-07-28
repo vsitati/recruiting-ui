bb6823812cca5e2d2ef1736b38e0208fe2884ddd
Jacques van der Westhuizen <jacqueswest@gmail.com>
2022-07-28 22:40:34 +0200

    fix: Moved validation to its own class.

--- test_data/test_data_details.py
+++ test_data/test_data_details.py
@@ -1,8 +1,9 @@
 from test_data.company_data import CompanyData
 from test_data.job_data import JobData
+from test_data.validation_data import ValidationData
 
 
-class TestData(CompanyData):
+class TestData(CompanyData, ValidationData):
     pass
 
 
