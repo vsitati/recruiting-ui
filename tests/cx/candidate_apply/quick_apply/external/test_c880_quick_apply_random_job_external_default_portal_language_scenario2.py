41bd84e1da25d247e4249417841513d56c0dc034
npedone <nicole.pedone@silkroad.com>
2022-12-09 10:31:36 -0500

    Testcase for Quick apply random job external Default Language Scenario 2. Also updated other default language tc as it wasn't running properly.

--- tests/cx/candidate_apply/quick_apply/external/test_c880_quick_apply_random_job_external_default_portal_language_scenario2.py
+++ tests/cx/candidate_apply/quick_apply/external/test_c880_quick_apply_random_job_external_default_portal_language_scenario2.py
@@ -12,9 +12,9 @@ from config import Config
 
 
 @pytest.mark.usefixtures("setup")
-class TestQuickApplyRandomJobExternalDefaultPortalLanguage:
-    @allure.description("Random Job Quick Apply External Default Portal Language")
-    def test_random_job_quick_apply_external_default_portal_language(self, get_test_info):
+class TestQuickApplyRandomJobExternalDefaultPortalLanguageScenario2:
+    @allure.description("Random Job Quick Apply External Default Portal Language Scenario 2")
+    def test_random_job_quick_apply_external_default_portal_language_scenario2(self, get_test_info):
         language = "english"
         login = Login(driver=self.driver)
         login.do_login(env_info=get_test_info)
@@ -36,7 +36,8 @@ class TestQuickApplyRandomJobExternalDefaultPortalLanguage:
 
         css.open_setting(setting="languages")
         ml = ManageLanguages(driver=self.driver)
-        ml.set_given_langauge_to_default_only(language=language, enable=True)
+        ml.enable_language("english", enable=True)
+        ml.enable_language("french", enable=True)
         ml.click_language_setting_save_btn()
 
         # start a new driver, so we can confirm in different browser language
@@ -44,9 +45,9 @@ class TestQuickApplyRandomJobExternalDefaultPortalLanguage:
 
         config = Config.env_config
         driver2 = Drivers.get_driver(config, "french")
-        cs2 = CareerSites(driver=driver2)
-        cs2.open_url(portal_url)
-        assert cs2.get_title() == "QA Automation Only - SilkRoad Talent Activation"
+        cs = CareerSites(driver=driver2)
+        cs.open_url(portal_url)
+        assert cs.get_title() == "QA Automation Only - SilkRoad Talent Activation"
 
         js = JobSearch(driver=driver2)
         job_elem, job_title = js.find_job(random_job=True)
@@ -55,7 +56,7 @@ class TestQuickApplyRandomJobExternalDefaultPortalLanguage:
 
         qa = QuickApply(driver=driver2)
         qa.click_cx_job_apply_btn()
-        assert qa.get_file_upload_instructions_text() == "Upload a doc, docx, htm, html, odt, pdf, rtf, or txt " \
-                                                         "file. Attachment must be less than 10 MB."
+        assert qa.get_file_upload_instructions_text() == "Téléchargez un fichier doc, docx, htm, html, odt, pdf, " \
+                                                         "rtf ou txt. La pièce jointe doit être inférieure à 10 Mo."
 
         driver2.quit()
