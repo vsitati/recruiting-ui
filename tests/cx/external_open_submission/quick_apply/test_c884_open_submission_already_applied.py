a1482b1ba49a9adec3475ade1a5e873282020141
vsitati <vsitati@silkroadtech.com>
2023-01-19 19:03:47 +0300

    Quick Apply - Open Submission - Spanish and refactored test c884

---
+++ tests/cx/external_open_submission/quick_apply/test_c884_open_submission_already_applied.py
@@ -0,0 +1,65 @@
+
+import allure
+import pytest
+
+from config import Config
+from cx_pages.career_site_settings.career_site_settings import CareerSiteSettings
+from cx_pages.jobs_search import JobSearch
+from cx_pages.career_site_settings.manage_general_settings import ManageGeneralSettings
+from cx_pages.career_site_settings.manage_languages import ManageLanguages
+from cx_pages.career_sites import CareerSites
+from cx_pages.cx_quick_apply import QuickApply
+from cx_pages.login import Login
+from test_data.test_data_details import SrTestData
+
+
+@pytest.mark.usefixtures("setup")
+class TestQuickApplyOpenSubmissionAlreadySubmitted:
+    @allure.description("Quick Apply - Open Submission - Already Submitted")
+    def test_quick_apply_open_submission_already_submitted(self, get_test_info):
+        language = "english"
+        login = Login(driver=self.driver)
+        login.do_login(env_info=get_test_info)
+        cs = CareerSites(driver=self.driver)
+        data = cs.get_career_sites(site_section="external")
+        result = cs.filter_career_site(data=data, site_name="Corporate Career Portal")
+        name, portal_url, settings_url = result
+        cs.open_url(settings_url)
+        # Career Site Settings
+        css = CareerSiteSettings(driver=self.driver)
+        css.open_setting(setting="general")
+
+        # Manage Settings
+        mgs = ManageGeneralSettings(driver=self.driver)
+        mgs.change_portal_default_language(language=language)
+        mgs.click_cx_settings_save_btn()
+
+        css.open_setting(setting="languages")
+        ml = ManageLanguages(driver=self.driver)
+        ml.set_given_langauge_to_default_only(language=language, enable=True)
+        ml.click_language_setting_save_btn()
+        cs.open_url(portal_url)
+        assert cs.get_title() == "QA Automation Only - SilkRoad Talent Activation"
+
+        js = JobSearch(driver=self.driver)
+        assert js.get_submit_resume_message() == "Not finding the perfect opportunity? Submit Your Resume/CV."
+        os_link = js.get_all_hrefs(specific_href="QuickApply")
+        js.open_url(os_link)
+
+        qa = QuickApply(driver=self.driver)
+        td = SrTestData()
+        form_details = td.get_quick_apply_form_data(parent_folder=Config.env_config["path_to_resumes"])
+        qa.fill_in_quick_apply_form(**form_details)
+        assert qa.get_success_message() == "Thank You for Submitting Your Resume/CV"
+        os2_link = js.get_all_hrefs(specific_href="SaveSearch")
+        js.open_url(os2_link)
+
+        # applying for second job
+        js = JobSearch(driver=self.driver)
+        os_link = js.get_all_hrefs(specific_href="QuickApply")
+        js.open_url(os_link)
+
+        qa2 = QuickApply(driver=self.driver)
+        qa2.fill_in_quick_apply_form(**form_details)
+
+        assert qa.already_applied_info() == "Already Submitted"
