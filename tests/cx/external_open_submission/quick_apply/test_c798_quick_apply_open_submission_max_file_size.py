1262337dda8824b3a578032601537bbdc6567415
npedone <nicole.pedone@silkroad.com>
2023-01-20 09:16:28 -0500

    Quick Apply Open submission max file size

---
+++ tests/cx/external_open_submission/quick_apply/test_c798_quick_apply_open_submission_max_file_size.py
@@ -0,0 +1,53 @@
+import pytest
+import allure
+from cx_pages.career_sites import CareerSites
+from cx_pages.jobs_search import JobSearch
+from cx_pages.login import Login
+from config import Config
+from cx_pages.cx_quick_apply import QuickApply
+from test_data.test_data_details import SrTestData
+from cx_pages.career_site_settings.manage_general_settings import ManageGeneralSettings
+from cx_pages.career_site_settings.career_site_settings import CareerSiteSettings
+from cx_pages.career_site_settings.manage_languages import ManageLanguages
+
+
+@pytest.mark.usefixtures("setup")
+class TestQuickApplyOpenSubmissionMaxFileSize:
+    @allure.description("Quick Apply Open Submission Max File Size")
+    def test_random_job_quick_apply_open_submission_max_file_size(self, get_test_info):
+        language = "english"
+        login = Login(driver=self.driver)
+        login.do_login(env_info=get_test_info)
+
+        cs = CareerSites(driver=self.driver)
+        data = cs.get_career_sites(site_section="external")
+        result = cs.filter_career_site(data=data, site_name="Corporate Career Portal")
+        name, portal_url, settings_url = result
+        cs.open_url(settings_url)
+
+        # Career Site Settings
+        css = CareerSiteSettings(driver=self.driver)
+        css.open_setting(setting="general")
+
+        # Manage Settings
+        mgs = ManageGeneralSettings(driver=self.driver)
+        mgs.change_portal_default_language(language=language)
+        mgs.click_cx_settings_save_btn()
+
+        css.open_setting(setting="languages")
+        ml = ManageLanguages(driver=self.driver)
+        ml.set_given_langauge_to_default_only(language=language, enable=True)
+        ml.click_language_setting_save_btn()
+        cs.open_url(portal_url)
+        assert cs.get_title() == "QA Automation Only - SilkRoad Talent Activation"
+
+        js = JobSearch(driver=self.driver)
+        assert js.get_submit_resume_message() == "Not finding the perfect opportunity? Submit Your Resume/CV."
+        os_link = js.get_all_hrefs(specific_href="QuickApply")
+        js.open_url(os_link)
+
+        qa = QuickApply(driver=self.driver)
+        td = SrTestData()
+        form_details = td.get_quick_apply_form_data(parent_folder=Config.env_config["path_to_large_files"])
+        qa.fill_in_quick_apply_form(**form_details)
+        assert qa.get_file_error() == "Your Resume/CV is too large. Please upload a file smaller than 10 MB."
