5fb07188044955540e22ea7f65b4f6a2632213a2
Jacques van der Westhuizen <jacqueswest@gmail.com>
2023-01-24 01:04:25 +0200

    readded candidate data

--- test_data/test_data_details.py
+++ test_data/test_data_details.py
@@ -1,5 +1,6 @@
 from test_data.company_data import CompanyData
 from test_data.job_data import JobData
+from test_data.candidate_data import CandidateData
 from test_data.validation_data import ValidationData
 from test_data.cx_candidate_apply.quick_apply.quick_apply_form_data import QuickApplyForm
 from test_data.cx_candidate_apply.portal_langauge_text import PortalLanguageTextData
