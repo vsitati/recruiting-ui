5593c8e29510e3402a8f48880d642203beee1d26
silkroadtech\jzhuang <jason.zhuang@silkroad.com>
2023-01-25 15:27:57 -0500

    modify directory

---
+++ tests/ats/candidates/test_candidate_quick_search.py
@@ -0,0 +1,65 @@
+import pytest
+import allure
+from ats_pages.login.login import Login
+from ats_pages.page_header import PageHeader
+from ats_pages.candidates.advanced_search import CandidateAdvancedSearch
+from helpers.utils import BaseError
+from common.common import Common
+from time import sleep
+
+
+@pytest.mark.usefixtures("setup")
+class TestCandidateQuickSearch:
+
+    @allure.title("Candidate Quick Search for All")
+    @allure.description("JIRA: RND-7463; TestRail: C296")
+    def test_candidate_quick_search_all(self, get_test_info):
+        login = Login(driver=self.driver)
+        login.do_login(get_test_info)
+
+        page_header = PageHeader(self.driver)
+        page_header.quick_search("Candidates")
+        sleep(page_header.sleep_time)
+
+        candidate_advanced_search = CandidateAdvancedSearch(self.driver)
+        candidate_advanced_search.verify_is_candidate_search_result_page()
+        record_count = candidate_advanced_search.verify_record_count()
+        if int(record_count) == 0:
+            raise BaseError(f"Search result should return some records, but only {record_count}.")
+        else:
+            Common.sr_logger.logger.info(f"-- Candidate Search returns {record_count} records.")
+
+    @allure.title("Candidate Quick Search for Some")
+    @allure.description("JIRA: RND-7464; TestRail: C298")
+    def test_candidate_quick_search_some(self, get_test_info):
+        login = Login(driver=self.driver)
+        login.do_login(get_test_info)
+
+        candidate_name = "Aaron"
+        page_header = PageHeader(self.driver)
+        page_header.quick_search("Candidates", candidate_name)
+
+        candidate_advanced_search = CandidateAdvancedSearch(self.driver)
+        candidate_advanced_search.verify_is_candidate_search_result_page()
+        col_list = candidate_advanced_search.get_candidate_column_values("Candidate")
+        candidate_advanced_search.verify_value_exist(candidate_name, col_list)
+        return
+
+    @allure.title("Candidate Quick Search for All")
+    @allure.description("JIRA: RND-7465; TestRail: C297")
+    def test_candidate_quick_search_none(self, get_test_info):
+        login = Login(driver=self.driver)
+        login.do_login(get_test_info)
+
+        candidate_name = "xyz"
+        page_header = PageHeader(self.driver)
+        page_header.quick_search("Candidates", candidate_name)
+        sleep(page_header.sleep_time)
+
+        candidate_advanced_search = CandidateAdvancedSearch(self.driver)
+        candidate_advanced_search.verify_is_candidate_search_result_page()
+        record_count = candidate_advanced_search.verify_record_count()
+        if int(record_count) == 0:
+            Common.sr_logger.logger.info("-- Candidate Search returns 0 record correctly.")
+        else:
+            raise BaseError(f"Search result should be 0, but returns {record_count}.")
