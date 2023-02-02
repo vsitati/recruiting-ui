545009cd5cf92b8c387e1491c296dc51b9a23a15
vsitati <vsitati@silkroadtech.com>
2023-02-03 01:01:24 +0300

    External Portal Configured apply Random Job

---
+++ tests/cx/External_Portal_Configured_Apply/test_c813_Configured_Apply_External Portal_Random_Job.py
@@ -0,0 +1,65 @@
+import allure
+import pytest
+
+from cx_pages.career_site_settings.career_site_settings import CareerSiteSettings
+from cx_pages.career_sites import CareerSites
+from cx_pages.cx_quick_apply import QuickApply
+from cx_pages.jobs_search import JobSearch
+from cx_pages.login import Login
+from test_data.test_data_details import SrTestData
+
+
+@pytest.mark.usefixtures("setup")
+class TestQuickApplyOpenSubmissionDefaultPortalLanguage:
+    @allure.description("Quick Apply Open Submission Default Portal Language")
+    def test_random_job_quick_apply_open_submission_default_portal_language(self, get_test_info):
+        language = "english"
+        login = Login(driver=self.driver)
+        login.do_login(env_info=get_test_info)
+
+        cs = CareerSites(driver=self.driver)
+        data = cs.get_career_sites(site_section="external")
+        result = cs.filter_career_site(data=data, site_name="Corporate Career Portal")
+        name, portal_url, settings_url = result
+        cs.open_url(settings_url)
+
+        # Career Site Settings
+        css = CareerSiteSettings(driver=self.driver)
+        css.open_setting(setting="application_form")
+        css.custom_apply_form_switch()
+        os_link = css.get_all_hrefs(specific_href="ApplicationForm")
+        css.open_url(os_link)
+        css.click_add_options_button()
+        css.opt_group_select()
+
+        # js = JobSearch(driver=self.driver)
+        # job_elem, job_title = js.find_job(random_job=True)
+        # js.open_job(job_elem=job_elem)
+        # assert job_title in js.get_title()
+        #
+        # qa = QuickApply(driver=self.driver)
+        # td = SrTestData()
+        # form_details = td.get_quick_apply_form_data(parent_folder=Config.env_config["path_to_resumes"])
+        # qa.click_cx_job_apply_btn()
+        # qa.fill_in_quick_apply_form(**form_details)
+        # assert qa.get_success_message() == "Thank You for Applying"
+        #
+        # # Login to ATS
+        # ats_login = AtsLogin(driver=self.driver)
+        # ats_login.do_login(get_test_info)
+        #
+        # left_menu = LeftMenus(driver=self.driver)
+        # left_menu.click_left_nav(left_menu.candidates)
+        # left_menu.click_left_nav(left_menu.candidates_advanced_search)
+        # cas = CandidateAdvancedSearch(driver=self.driver)
+        # candidate_name = f"{form_details.get('firstname')} {form_details.get('lastname')}"
+        # cas.open_candidate_profile(candidate_name=candidate_name)
+        #
+        # crp = CandidateResumeProfile(driver=self.driver)
+        # assert crp.verify_candidate_name() == candidate_name
+        # assert crp.verify_candidate_email() == f"{form_details.get('email')}"
+        #
+        # crp.open_attachment_tab()
+        # crp.get_attachment_names()
+        # attachments = crp.get_attachment_names()
+        # assert get_basename_from_file_path(file_path=form_details.get("file_path")) in
