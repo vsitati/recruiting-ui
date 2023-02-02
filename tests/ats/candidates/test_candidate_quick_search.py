3a6254568f09d0e4120e3cd2cd33480d88597895
silkroadtech\jzhuang <jason.zhuang@silkroad.com>
2023-02-02 11:18:09 -0500

    ats quick_search

--- tests/ats/candidates/test_candidate_quick_search.py
+++ tests/ats/candidates/test_candidate_quick_search.py
@@ -43,7 +43,6 @@ class TestCandidateQuickSearch:
         candidate_advanced_search.verify_value_exist(candidate_name, col_list)
         return
 
-    @pytest.mark.skip(reason="Jason to fix")
     @allure.title("Candidate Quick Search for All")
     @allure.description("JIRA: RND-7465; TestRail: C297")
     def test_candidate_quick_search_none(self, get_test_info):
