ce751c1e2349a0ab12c4ed632a511249b27e7125
Jacques van der Westhuizen <jacqueswest@gmail.com>
2023-02-04 01:43:49 +0200

    unskipped tests

--- tests/ats/login/test_c755_ats_sso_login.py
+++ tests/ats/login/test_c755_ats_sso_login.py
@@ -6,7 +6,6 @@ from test_data.test_data_details import SrTestData
 
 @pytest.mark.usefixtures("setup")
 class TestAtsSsoLogin:
-    @pytest.mark.skip(reason="User not setup in env")
     @allure.title("C755 - ATS SSO Login Tests")
     @allure.description("Login via SSO with a valid user")
     def test_can_login_with_active_sso_user(self, get_test_info):
@@ -20,7 +19,6 @@ class TestAtsSsoLogin:
 
         assert login.is_element_visible(locator=login.quick_search) is True
 
-    @pytest.mark.skip(reason="User not setup in env")
     @allure.description("Scenario Login via SSO with a invalid/non-existing user")
     def test_cannot_login_with_an_invalid_sso_user(self, get_test_info):
         username, password = SrTestData.data[get_test_info.get("company")]["users"]["sso_invalid_username"]
@@ -31,7 +29,6 @@ class TestAtsSsoLogin:
         error_msg = login.get_text(locator=login.sso_login_error)
         assert error_msg == SrTestData.sso_validation.get("sso_invalid_username_error", "")
 
-    @pytest.mark.skip(reason="User not setup in env")
     @allure.description("Scenario Login via SSO with an inactive user with SSO enabled")
     def test_cannot_login_with_an_inactive_sso_user(self, get_test_info):
         username, password = SrTestData.data[get_test_info.get("company")]["users"]["sso_inactive_username"]
@@ -42,7 +39,6 @@ class TestAtsSsoLogin:
         error_msg = login.get_text(locator=login.sso_login_error)
         assert error_msg == SrTestData.sso_validation.get("sso_inactive_user_error", "")
 
-    @pytest.mark.skip(reason="User not setup in env")
     @allure.description("Login via SSO with an active user with SSO disabled")
     def test_cannot_login_with_an_active_sso_disabled_user(self, get_test_info):
         username, password = SrTestData.data[get_test_info.get("company")]["users"]["sso_disabled_active_username"]
@@ -53,7 +49,6 @@ class TestAtsSsoLogin:
         error_msg = login.get_text(locator=login.sso_login_error)
         assert error_msg == SrTestData.sso_validation.get("sso_inactive_user_error", "")
 
-    @pytest.mark.skip(reason="User not setup in env")
     @allure.description("Login via SSO with an inactive user with SSO disabled")
     def test_cannot_login_with_an_inactive_sso_disabled_user(self, get_test_info):
         username, password = SrTestData.data[get_test_info.get("company")]["users"]["sso_disabled_inactive_username"]
