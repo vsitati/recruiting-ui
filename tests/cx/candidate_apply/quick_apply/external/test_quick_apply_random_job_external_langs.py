15c5259900f1388e24b248ec7486bee6972c28c4
Jacques van der Westhuizen <jacqueswest@gmail.com>
2023-02-05 20:02:36 +0200

    group E

--- tests/cx/candidate_apply/quick_apply/external/test_quick_apply_random_job_external_langs.py
+++ tests/cx/candidate_apply/quick_apply/external/test_quick_apply_random_job_external_langs.py
@@ -16,6 +16,7 @@ from cx_pages.career_site_settings.career_site_settings import CareerSiteSetting
 from cx_pages.career_site_settings.manage_languages import ManageLanguages
 
 
+@pytest.mark.regression_grp_e
 @pytest.mark.usefixtures("setup")
 class TestQuickApplyRandomJobExternalLangs:
     @pytest.mark.parametrize("language", ["french", "spanish", "german"])
