31515dc7893eaec62c678d50995c43ee771adba9
Jacques van der Westhuizen <jacqueswest@gmail.com>
2023-02-07 17:54:44 +0200

    Fixing flaky tests. Introduced a db to share data between tests

--- ats_pages/jobs/job_evaluation_questions.py
+++ ats_pages/jobs/job_evaluation_questions.py
@@ -5,11 +5,12 @@ from test_data.test_data_details import JobData
 
 
 class Elements:
-    select_checkbox = "evalquestionid"
+    select_checkbox = (By.NAME, "evalquestionid")
     select_question_quickly_btn = (By.ID, "selectQuestionsQuicklyButton")
     update_question_settings_btn = (By.ID, "updateQuestionSettingsButton")
     return_to_job_lists_btn = (By.CSS_SELECTOR, ".lifesuite__button.lifesuite__float-right")
     save_btn = (By.XPATH, "//span[text()='Save']")
+    link_text_elem = (By.LINK_TEXT, JobData.job_data.get("cqe"))
 
 
 class JobEvaluationQuestions(Common, Elements):
@@ -18,17 +19,16 @@ class JobEvaluationQuestions(Common, Elements):
 
     def select_cqe(self):
         # open the link
-        text = JobData.job_data.get("cqe")
-        elm_link = self.driver.find_element_by_locator((By.LINK_TEXT, text))
+        elm_link = self.driver.find_element_by_locator(self.link_text_elem)
         self.do_click(elm_link)
         # check a checkbox
-        elm_checkbox = self.driver.find_element(locate_with(By.NAME, self.select_checkbox).below(elm_link))
+        elm_checkbox = self.driver.find_element(locate_with(*self.select_checkbox).below(elm_link))
         self.do_click(elm_checkbox)
         # click select question quickly button
         self.go_click(self.select_question_quickly_btn)
         # click return to job list button
         elm_btn = self.driver.find_element_by_locator(self.update_question_settings_btn)
-        elm = self.driver.find_element(locate_with(By.CSS_SELECTOR, self.return_to_job_lists_btn).to_left_of(elm_btn))
+        elm = self.driver.find_element(locate_with(*self.return_to_job_lists_btn).to_left_of(elm_btn))
         self.do_click(elm)
 
     def update_cqe(self):
@@ -37,7 +37,7 @@ class JobEvaluationQuestions(Common, Elements):
         elm_link = self.driver.find_element_by_locator((By.LINK_TEXT, text))
         self.do_click(elm_link)
         # check a checkbox
-        elm_checkbox = self.driver.find_element(locate_with(By.NAME, self.select_checkbox).below(elm_link))
+        elm_checkbox = self.driver.find_element(locate_with(*self.select_checkbox).below(elm_link))
         self.do_click(elm_checkbox)
         # click return to job list button
         elm_btn = self.driver.find_element_by_locator(self.update_question_settings_btn)
@@ -46,7 +46,7 @@ class JobEvaluationQuestions(Common, Elements):
         self.go_click(self.save_btn)
 
         elm_btn = self.driver.find_element_by_locator(self.update_question_settings_btn)
-        elm = self.driver.find_element(locate_with(By.CSS_SELECTOR, self.return_to_job_lists_btn).to_left_of(elm_btn))
+        elm = self.driver.find_element(locate_with(*self.return_to_job_lists_btn).to_left_of(elm_btn))
         self.do_click(elm)
 
     def click_return_to_job_lists_btn(self):
